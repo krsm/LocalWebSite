2a39fa264fd10eb303a15c4ca36242c46364fb0d
krsm <krsm184@gmail.com>
2017-02-12 09:09:18 -0500

    rename folders

--- payment_sytem/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PaymentSytemConfig(AppConfig):
-    name = 'payment_sytem'
--- user_registration/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserRegistrationConfig(AppConfig):
-    name = 'user_registration'
