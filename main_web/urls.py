e286928d2a58c364cd033d5168cbf4f23644cd4f
krsm <krsm184@gmail.com>
2017-02-13 19:33:32 -0500

    added basic structure of user login, and register

--- main_web/urls.py
+++ main_web/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    # route related to admin
     url(r'^admin/', admin.site.urls),
+
+    # to include users urls
+    url(r'^users/', include('music.urls')),
 ]
