37dd4cc64a406dd1f4d87b6225bcc43f6e93de44
krsm <krsm184@gmail.com>
2017-04-09 23:01:46 -0400

    attempt to display image, and added get_models to views

--- localthisandthat/core/views.py
+++ localthisandthat/core/views.py
@@ -1,11 +1,20 @@
+
+from django.apps import apps
+
+
 from django.shortcuts import render
 from django.http import HttpResponse
 # import models related to suppliers app
 # to be used to display info related products
-from ..suppliers.models import Product
+# from ..suppliers.models import Product
+
 
 # Create your views here.
 # Landing Page - Main Page
 def home_page(request):
-    test_html = {'test': 'test'}
-    return render(request, 'main_page.html', test_html)
+    # getting the Products Model
+    products_model = apps.get_model('suppliers', 'Product')
+    products = products_model.objects.all().order_by('-id')
+    # print(products)
+    test_html = {'products': products}
+    return render(request, 'core/main_page.html', test_html)
